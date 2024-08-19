f0704a85df3fdaef6b01c991fc103e2dd464ce7c
p1n9d3v <p1n9m4i1@gmail.com>
2024-08-19 14:50:12 +0900

    feat(config.fish) add alias about git and commitizen

--- config.fish
+++ config.fish
@@ -1,3 +1,7 @@
+# set default node version
+
+nvm use v18
+
 # set color
 set fish_color_command 77E4C8 --bold
 
@@ -7,6 +11,10 @@ alias ll "eza -l --icons"
 alias lla "eza -l -a --icons"
 alias llat "eza -l -a --tree --icons"
 alias vim nvim
+alias g git
+alias gaa "git add -A"
+alias gcz "git cz -a"
+alias gl "git log"
 
 
 # fzf key bindings
