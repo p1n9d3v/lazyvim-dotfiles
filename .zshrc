daf32dd10c56f9f7f1f0983ba89d6a722dd9f56e
p1n9d3v <p1n9m4i1@gmail.com>
2024-09-25 08:41:50 +0900

    🐛 fix: updated lua and .zshrc

--- .zshrc
+++ .zshrc
@@ -13,6 +13,12 @@ alias ls="eza --icons"
 alias ll="eza -l --icons"
 alias lla="eza -l -a --icons"
 alias llat="eza -l -a --tree --icons"
+alias g=git
+alias gaa="git add -A"
+alias gst="git status"
+alias gcz="git cz -a"
+alias gcf="git commit --amend"
+alias gl="git log"
 
 findandkill() {
   $(lsof -ti:3000 | xargs kill)
@@ -24,3 +30,11 @@ alias kill_port=findandkill
 
 source /home/p1n9d3v/.oh-my-zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
 source /home/p1n9d3v/.oh-my-zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+
+# pnpm
+export PNPM_HOME="/home/p1n9d3v/.local/share/pnpm"
+case ":$PATH:" in
+  *":$PNPM_HOME:"*) ;;
+  *) export PATH="$PNPM_HOME:$PATH" ;;
+esac
+# pnpm end
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,49 +1,52 @@
 vim.lsp.handlers["textDocument/hover"] = function(_, result, ctx, config)
-  config = config
-    or {
-      border = {
-        { "╭", "Comment" },
-        { "─", "Comment" },
-        { "╮", "Comment" },
-        { "│", "Comment" },
-        { "╯", "Comment" },
-        { "─", "Comment" },
-        { "╰", "Comment" },
-        { "│", "Comment" },
-      },
-    }
-  config.focus_id = ctx.method
-  if not (result and result.contents) then
-    return
-  end
-  local markdown_lines = vim.lsp.util.convert_input_to_markdown_lines(result.contents)
-  markdown_lines = vim.lsp.util.trim_empty_lines(markdown_lines)
-  if vim.tbl_isempty(markdown_lines) then
-    return
-  end
-  return vim.lsp.util.open_floating_preview(markdown_lines, "markdown", config)
+    config = config
+        or {
+            border = {
+                { "╭", "Comment" },
+                { "─", "Comment" },
+                { "╮", "Comment" },
+                { "│", "Comment" },
+                { "╯", "Comment" },
+                { "─", "Comment" },
+                { "╰", "Comment" },
+                { "│", "Comment" },
+            },
+        }
+    config.focus_id = ctx.method
+    if not (result and result.contents) then
+        return
+    end
+    local markdown_lines = vim.lsp.util.convert_input_to_markdown_lines(result.contents)
+    markdown_lines = vim.lsp.util.trim_empty_lines(markdown_lines)
+    if vim.tbl_isempty(markdown_lines) then
+        return
+    end
+    return vim.lsp.util.open_floating_preview(markdown_lines, "markdown", config)
 end
 
 return {
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        vtsls = {
-          settings = {
-            typescript = {
-              inlayHints = {
-                enumMemberValues = { enabled = false },
-                functionLikeReturnTypes = { enabled = false },
-                parameterNames = { enabled = false },
-                parameterTypes = { enabled = false },
-                propertyDeclarationTypes = { enabled = false },
-                variableTypes = { enabled = false },
-              },
+    {
+        "neovim/nvim-lspconfig",
+        opts = {
+            servers = {
+                vtsls = {
+                    settings = {
+                        typescript = {
+                            preferences = {
+                                importModuleSpecifier = "non-relative",
+                            },
+                            inlayHints = {
+                                enumMemberValues = { enabled = false },
+                                functionLikeReturnTypes = { enabled = false },
+                                parameterNames = { enabled = false },
+                                parameterTypes = { enabled = false },
+                                propertyDeclarationTypes = { enabled = false },
+                                variableTypes = { enabled = false },
+                            },
+                        },
+                    },
+                },
             },
-          },
         },
-      },
     },
-  },
 }
